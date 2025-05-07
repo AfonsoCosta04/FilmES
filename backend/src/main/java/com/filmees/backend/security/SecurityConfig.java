aa1278f182321006f513167f4c13387261046d49
AfonsoCosta04 <afonso.costa@ipcbcampus.pt>
2025-05-07 13:06:49 +0100

    commit

--- backend/src/main/java/com/filmees/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/filmees/backend/security/SecurityConfig.java
@@ -41,6 +41,19 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
 
 
