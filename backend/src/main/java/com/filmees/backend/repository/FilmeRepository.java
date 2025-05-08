770ad7173c390f3a1247c4ef092b59167ff43f2f
AfonsoCosta04 <afonso.costa@ipcbcampus.pt>
2025-05-08 18:22:18 +0100

    commit

--- backend/src/main/java/com/filmees/backend/repository/FilmeRepository.java
+++ backend/src/main/java/com/filmees/backend/repository/FilmeRepository.java
@@ -2,6 +2,7 @@ package com.filmees.backend.repository;
 
 import com.filmees.backend.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 // Filme Repository
