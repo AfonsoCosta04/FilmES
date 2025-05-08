5980c31d25b13151564cb344d9bae85b342d9b34
AfonsoCosta04 <afonso.costa@ipcbcampus.pt>
2025-05-08 18:22:03 +0100

    Commit

--- backend/src/main/java/com/filmees/backend/repository/FilmeRepository.java
+++ backend/src/main/java/com/filmees/backend/repository/FilmeRepository.java
@@ -7,4 +7,12 @@ import org.springframework.stereotype.Repository;
 // Filme Repository
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+    @Query("""
+    SELECT f FROM Filme f
+    JOIN Aluguer a ON f.idFilme = a.filme.idFilme
+    WHERE a.dataLevantamento >= CURRENT_DATE - 7
+    GROUP BY f.idFilme
+    ORDER BY COUNT(a.idAluguer) DESC
+""")
+    List<Filme> findMaisAlugadosUltimaSemana();
 }
